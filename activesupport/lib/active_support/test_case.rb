4f406872a95a6b1015ba03e9ceb3a90a6bbdaab9
Ryan Davis <ryand-ruby@zenspider.com>
2013-05-01 16:12:34 -0700

    Override Minitest.__run to sort test classes by name. LAME. From source:
        # FIXME: MT5's runnables is not ordered. This is needed because
        # we have have tests have cross-class order-dependent bugs.

--- activesupport/lib/active_support/test_case.rb
+++ activesupport/lib/active_support/test_case.rb
@@ -16,6 +16,23 @@ begin
 rescue LoadError
 end
 
+module Minitest # :nodoc:
+  class << self
+    remove_method :__run
+  end
+
+  def self.__run reporter, options # :nodoc:
+    # FIXME: MT5's runnables is not ordered. This is needed because
+    # we have have tests have cross-class order-dependent bugs.
+    suites = Runnable.runnables.sort_by { |ts| ts.name.to_s }
+
+    parallel, serial = suites.partition { |s| s.test_order == :parallel }
+
+    ParallelEach.new(parallel).map { |suite| suite.run reporter, options } +
+     serial.map { |suite| suite.run reporter, options }
+  end
+end
+
 module ActiveSupport
   class TestCase < ::Minitest::Test
     Assertion = Minitest::Assertion
